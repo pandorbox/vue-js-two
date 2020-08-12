85ba8f9142248b435a359a628b31e6a8f0d9b602
qijiankan <admin@qq.com>
2020-08-12 12:01:14 +0800

    1.0

--- src/core/api/index.js
+++ src/core/api/index.js
@@ -1,15 +1,11 @@
-import Kecheng from "./kecheng";
 import Account from "./account";
 /**
  * api
  */
 export default class Index {
-  /** 课程 */
-  Kecheng;
   /** 登录 */
   Account;
   constructor() {
-    this.Kecheng = new Kecheng();
     this.Account = new Account();
   }
 }
